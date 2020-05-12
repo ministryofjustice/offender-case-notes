cb627bc1e1157c4a431602da3925dc3995360c68
petergphillips <peter@greenthistle.co.uk>
2020-05-12 08:37:45 +0100

    DT-813: Use improved auth helper (#86)
    * Rename .java to .kt
    
    * DT-813: Use improved auth helper

---
+++ src/test/java/uk/gov/justice/hmpps/casenotes/controllers/ResourceTest.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.hmpps.casenotes.controllers
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.hmpps.casenotes.health.wiremock.Elite2Extension
+import uk.gov.justice.hmpps.casenotes.health.wiremock.OAuthExtension
+import uk.gov.justice.hmpps.casenotes.utils.JwtAuthHelper
+import java.util.function.Consumer
+
+@DirtiesContext(classMode = BEFORE_CLASS)
+@ActiveProfiles("test", "noqueue")
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(Elite2Extension::class, OAuthExtension::class)
+abstract class ResourceTest {
+  @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+  @Autowired
+  internal lateinit var webTestClient: WebTestClient
+
+  @Autowired
+  internal lateinit var jwtHelper: JwtAuthHelper
+
+  fun addBearerAuthorisation(user: String, roles: List<String>?): Consumer<HttpHeaders> {
+    val jwt = createJwt(user, roles)
+    return addBearerToken(jwt)
+  }
+
+  fun addBearerToken(token: String): Consumer<HttpHeaders> = Consumer { headers: HttpHeaders ->
+    headers.add(HttpHeaders.AUTHORIZATION, "Bearer $token")
+    headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+    headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+  }
+
+  fun createJwt(user: String, roles: List<String>?): String = jwtHelper.createJwt(user, roles = roles)
+
+  fun readFile(file: String): String = this.javaClass.getResource(file).readText()
+}
