b2a4674ee8074da6ee24b36fabb2c6a142a2ccf4
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-05-28 12:00:48 +0100

    DT-877 useLatestVersions

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,12 @@ configurations {
 extra["spring-security.version"] = "5.3.2.RELEASE"
 
 dependencies {
-  annotationProcessor("org.projectlombok:lombok:1.18.8")
+  annotationProcessor("org.projectlombok:lombok:1.18.12")
 
-  compileOnly("org.projectlombok:lombok:1.18.8")
+  compileOnly("org.projectlombok:lombok:1.18.12")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:6.4.1")
+  runtimeOnly("org.flywaydb:flyway-core:6.4.3")
   runtimeOnly("org.postgresql:postgresql:42.2.12")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -47,17 +47,17 @@ dependencies {
   implementation("com.pauldijou:jwt-core_2.11:4.3.0")
   implementation("com.google.code.gson:gson:2.8.6")
 
-  implementation("software.amazon.awssdk:sns:2.13.13")
+  implementation("software.amazon.awssdk:sns:2.13.25")
 
-  testAnnotationProcessor("org.projectlombok:lombok:1.18.8")
-  testCompileOnly("org.projectlombok:lombok:1.18.8")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.12")
+  testCompileOnly("org.projectlombok:lombok:1.18.12")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.17.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.26.3")
   testImplementation("org.testcontainers:localstack:1.13.0")
-  testImplementation("org.awaitility:awaitility-kotlin:4.0.2")
+  testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
 
   testImplementation("org.springframework.security.oauth:spring-security-oauth2:2.4.1.RELEASE")
