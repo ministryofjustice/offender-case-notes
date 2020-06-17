10f8be3ac96f5ebc96fc28c35634f952bb7a21a9
Peter Phillips <peter@greenthistle.co.uk>
2020-06-17 10:10:39 +0100

    DT-926: Switch master to main and upgrade spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.2"
   kotlin("plugin.spring") version "1.3.72"
 }
 
@@ -7,10 +7,7 @@ configurations {
   implementation { exclude(mapOf("module" to "tomcat-jdbc")) }
 }
 
-extra["spring-security.version"] = "5.3.2.RELEASE"
-
 dependencies {
-
   annotationProcessor("org.projectlombok:lombok:1.18.12")
 
   compileOnly("org.projectlombok:lombok:1.18.12")
@@ -48,16 +45,16 @@ dependencies {
   implementation("com.pauldijou:jwt-core_2.11:4.3.0")
   implementation("com.google.code.gson:gson:2.8.6")
 
-  implementation("software.amazon.awssdk:sns:2.13.34")
+  implementation("software.amazon.awssdk:sns:2.13.38")
 
   testAnnotationProcessor("org.projectlombok:lombok:1.18.12")
   testCompileOnly("org.projectlombok:lombok:1.18.12")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.17.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.18.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.26.3")
-  testImplementation("org.testcontainers:localstack:1.13.0")
+  testImplementation("org.testcontainers:localstack:1.14.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
 
