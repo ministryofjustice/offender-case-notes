14289f7118530e0516e4c6a09e1b24684ca94e5a
Peter Phillips <peter@greenthistle.co.uk>
2021-01-05 15:29:45 +0000

    DT-1442: ✅ Fix tests after insights changes

--- src/main/java/uk/gov/justice/hmpps/casenotes/config/ClientTrackingInterceptor.kt
+++ src/main/java/uk/gov/justice/hmpps/casenotes/config/ClientTrackingInterceptor.kt
@@ -5,17 +5,20 @@ import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.SignedJWT
 import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import uk.gov.justice.hmpps.casenotes.config.ApplicationInsightsConfiguration.AppInsightsConfigurationPresent
 import java.text.ParseException
 import java.util.Optional
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Configuration
+@ConditionalOnBean(AppInsightsConfigurationPresent::class)
 class ClientTrackingConfiguration(private val clientTrackingInterceptor: ClientTrackingInterceptor) : WebMvcConfigurer {
   override fun addInterceptors(registry: InterceptorRegistry) {
     registry.addInterceptor(clientTrackingInterceptor).addPathPatterns("/**")
@@ -24,7 +27,7 @@ class ClientTrackingConfiguration(private val clientTrackingInterceptor: ClientT
 
 @Configuration
 class ClientTrackingInterceptor : HandlerInterceptor {
-  override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, D: Any): Boolean {
+  override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
     val token = request.getHeader(HttpHeaders.AUTHORIZATION)
     val bearer = "Bearer "
     if (StringUtils.startsWithIgnoreCase(token, bearer)) {
