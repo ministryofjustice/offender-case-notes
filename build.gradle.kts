4da7d812ccbd885f348c4a403a30647142a88cbb
Peter Phillips <peter@greenthistle.co.uk>
2021-01-15 09:34:14 +0000

    DT-1442: ⬆️🚧 Try out application insights agent v3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.0.0-BETA"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.0.0-BETA-2"
   kotlin("plugin.spring") version "1.4.21"
 }
 
@@ -13,7 +13,7 @@ dependencies {
   compileOnly("org.projectlombok:lombok:1.18.16")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.4.0")
+  runtimeOnly("org.flywaydb:flyway-core:7.5.0")
   runtimeOnly("org.postgresql:postgresql:42.2.18")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -34,12 +34,12 @@ dependencies {
 
   implementation("net.sf.ehcache:ehcache:2.10.6")
   implementation("org.apache.commons:commons-text:1.9")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.0")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
   implementation("com.pauldijou:jwt-core_2.11:4.3.0")
   implementation("com.google.code.gson:gson:2.8.6")
 
-  implementation("software.amazon.awssdk:sns:2.15.58")
+  implementation("software.amazon.awssdk:sns:2.15.65")
 
   testAnnotationProcessor("org.projectlombok:lombok:1.18.16")
   testCompileOnly("org.projectlombok:lombok:1.18.16")
