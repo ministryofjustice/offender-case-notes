c6cddf10d515eb2151924c8c75b3e7892a3d7961
Peter Phillips <peter@greenthistle.co.uk>
2021-03-15 10:57:52 +0000

    DT-1522: ⬆️🐛 Try out latest app insights to fix incorrect correlation mapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
   compileOnly("org.projectlombok:lombok:1.18.18")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.5.3")
+  runtimeOnly("org.flywaydb:flyway-core:7.7.0")
   runtimeOnly("org.postgresql:postgresql:42.2.19")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -34,12 +34,12 @@ dependencies {
 
   implementation("net.sf.ehcache:ehcache:2.10.6")
   implementation("org.apache.commons:commons-text:1.9")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.1")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.2")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.8.6")
 
-  implementation("software.amazon.awssdk:sns:2.16.3")
+  implementation("software.amazon.awssdk:sns:2.16.18")
 
   testAnnotationProcessor("org.projectlombok:lombok:1.18.18")
   testCompileOnly("org.projectlombok:lombok:1.18.18")
@@ -52,4 +52,6 @@ dependencies {
 
   testImplementation("org.springframework.security.oauth:spring-security-oauth2:2.5.0.RELEASE")
   testImplementation("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
+
+  agentDeps("com.microsoft.azure:applicationinsights-agent:3.0.3-BETA")
 }
