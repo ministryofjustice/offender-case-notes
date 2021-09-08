27ab394d717d29a3e2af80f7cec5ab7bb1835dda
petergphillips <peter@greenthistle.co.uk>
2021-09-08 10:08:51 +0100

    DT-2510: 🔧 Add upgrade to Dockerfiles and upgrade libraries (#191)
    * DT-2510: 🔧 Add upgrade to Dockerfiles and upgrade libraries
    
    * DT-2510: 🔧 Also fix java / kotlin jdk versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
   compileOnly("org.projectlombok:lombok:1.18.20")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.9.0")
-  runtimeOnly("org.postgresql:postgresql:42.2.20")
+  runtimeOnly("org.flywaydb:flyway-core:7.15.0")
+  runtimeOnly("org.postgresql:postgresql:42.2.23")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-cache")
@@ -25,7 +25,7 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
 
-  implementation("com.google.code.gson:gson:2.8.7")
+  implementation("com.google.code.gson:gson:2.8.8")
   implementation("javax.activation:activation:1.1.1")
   implementation("javax.transaction:javax.transaction-api:1.3")
 
@@ -33,18 +33,18 @@ dependencies {
 
   implementation("net.sf.ehcache:ehcache:2.10.9.2")
   implementation("org.apache.commons:commons-text:1.9")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.4")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.4")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.5")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.8")
 
-  implementation("software.amazon.awssdk:sns:2.16.62")
+  implementation("software.amazon.awssdk:sns:2.17.34")
 
   testAnnotationProcessor("org.projectlombok:lombok:1.18.20")
   testCompileOnly("org.projectlombok:lombok:1.18.20")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
@@ -55,8 +55,12 @@ dependencies {
   testImplementation("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
 }
 
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+}
+
 tasks {
-  compileKotlin {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
       jvmTarget = "16"
     }
