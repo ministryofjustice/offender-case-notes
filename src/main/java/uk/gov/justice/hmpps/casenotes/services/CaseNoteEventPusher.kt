d2bda413e75c333b4694e10d32f00df32b23333f
Marcus Aspin <maspin@unilink.com>
2022-11-02 17:21:17 +0000

    PI-377 Updated to publish to the HMPPS Domain Events topic (#274)
    * PI-377 Updated to publish to the HMPPS Domain Events topic
    
    * Rename Spring property 'domain-events' to 'domainevents'

--- src/main/java/uk/gov/justice/hmpps/casenotes/services/CaseNoteEventPusher.kt
+++ src/main/java/uk/gov/justice/hmpps/casenotes/services/CaseNoteEventPusher.kt
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.commons.lang3.math.NumberUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import uk.gov.justice.hmpps.casenotes.dto.CaseNote
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.HmppsTopic
+import java.net.URI
 import java.time.LocalDateTime
 
 interface CaseNoteEventPusher {
@@ -24,27 +26,29 @@ interface CaseNoteEventPusher {
 @Component
 class CaseNoteAwsEventPusher(
   private val hmppsQueueService: HmppsQueueService,
-  private val objectMapper: ObjectMapper
+  private val objectMapper: ObjectMapper,
+  @Value("\${casenotes.api.base.url}") private val caseNotesApiBaseUrl: String,
 ) : CaseNoteEventPusher {
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 
-  internal val eventTopic by lazy { hmppsQueueService.findByTopicId("offenderevents") as HmppsTopic }
+  internal val eventTopic by lazy { hmppsQueueService.findByTopicId("domainevents") as HmppsTopic }
   internal val snsClient by lazy { eventTopic.snsClient }
   internal val topicArn by lazy { eventTopic.arn }
 
   override fun sendEvent(caseNote: CaseNote) {
     if (isSensitiveCaseNote(caseNote.caseNoteId)) {
-      val cne = CaseNoteEvent(caseNote)
-      log.info("Pushing case note {} to event topic with event type of {}", cne.caseNoteId, cne.eventType)
+      val cne = HmppsDomainEvent(caseNote, caseNotesApiBaseUrl)
+      log.info("Pushing case note {} to event topic with event type of {}", caseNote.caseNoteId, cne.eventType)
       val publishRequest = PublishRequest(
         topicArn,
         objectMapper.writeValueAsString(cne)
       ).withMessageAttributes(
         mapOf(
           "eventType" to MessageAttributeValue().withDataType("String").withStringValue(cne.eventType),
-          "contentType" to MessageAttributeValue().withDataType("String").withStringValue("text/plain;charset=UTF-8")
+          "contentType" to MessageAttributeValue().withDataType("String").withStringValue("text/plain;charset=UTF-8"),
+          "caseNoteType" to MessageAttributeValue().withDataType("String").withStringValue(cne.additionalInformation.caseNoteType),
         )
       )
 
@@ -58,18 +62,29 @@ class CaseNoteAwsEventPusher(
   }
 }
 
-data class CaseNoteEvent(
-  val eventType: String,
-  val eventDatetime: LocalDateTime,
-  val offenderIdDisplay: String,
-  val agencyLocationId: String,
-  val caseNoteId: String
+data class PersonIdentifier(val type: String, val value: String)
+data class PersonReference(val identifiers: List<PersonIdentifier>)
+data class CaseNoteAdditionalInformation(
+  val caseNoteId: String,
+  val caseNoteType: String,
+)
+
+data class HmppsDomainEvent(
+  val version: Int = 1,
+  val eventType: String = "prison.case-note.published",
+  val description: String = "A prison case note has been created or amended",
+  val detailUrl: String,
+  val occurredAt: LocalDateTime,
+  val personReference: PersonReference,
+  val additionalInformation: CaseNoteAdditionalInformation,
 ) {
-  constructor(cn: CaseNote) : this(
-    eventType = "${cn.type}-${cn.subType}",
-    eventDatetime = cn.creationDateTime,
-    offenderIdDisplay = cn.offenderIdentifier,
-    agencyLocationId = cn.locationId,
-    caseNoteId = cn.caseNoteId
+  constructor(caseNote: CaseNote, baseUrl: String) : this(
+    detailUrl = URI.create("$baseUrl/case-notes/${caseNote.offenderIdentifier}/${caseNote.caseNoteId}").toString(),
+    occurredAt = caseNote.creationDateTime,
+    personReference = PersonReference(identifiers = listOf(PersonIdentifier("NOMS", caseNote.offenderIdentifier))),
+    additionalInformation = CaseNoteAdditionalInformation(
+      caseNoteId = caseNote.caseNoteId,
+      caseNoteType = "${caseNote.type}-${caseNote.subType}",
+    )
   )
 }
