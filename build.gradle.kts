2654d123982b1127e98e1f71c6c1874204597e91
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-03-24 14:42:47 +0000

    HAAR-1244: ⬆️ update to spring-boot-plugin-4.8.5-beta (#295)
    * HAAR-1244: ⬆️ update to spring-boot-plugin-4.8.5-beta
    
    * HAAR-1244: ⬆️ update to java 19

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.4-beta-2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.5-beta"
   kotlin("plugin.spring") version "1.8.10"
 }
 
@@ -14,7 +14,7 @@ dependencies {
 
   runtimeOnly("com.h2database:h2:2.1.214")
   runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql:42.5.4")
+  runtimeOnly("org.postgresql:postgresql:42.6.0")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-cache")
@@ -29,9 +29,9 @@ dependencies {
   implementation("javax.activation:activation:1.1.1")
   implementation("javax.transaction:javax.transaction-api:1.3")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.14")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.14")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.15")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.15")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.15")
 
   implementation("net.sf.ehcache:ehcache:2.10.9.2")
   implementation("org.apache.commons:commons-text:1.10.0")
@@ -40,16 +40,16 @@ dependencies {
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.10.1")
 
-  implementation("software.amazon.awssdk:sns:2.20.17")
+  implementation("software.amazon.awssdk:sns:2.20.31")
 
-  implementation("io.swagger:swagger-core:1.6.9")
-  implementation("io.swagger.core.v3:swagger-core:2.2.8")
+  implementation("io.swagger:swagger-core:1.6.10")
+  implementation("io.swagger.core.v3:swagger-core:2.2.9")
 
   testAnnotationProcessor("org.projectlombok:lombok:1.18.26")
   testCompileOnly("org.projectlombok:lombok:1.18.26")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.1")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.37.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
@@ -63,13 +63,13 @@ dependencies {
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(18))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "18"
+      jvmTarget = "19"
     }
   }
 }
