aeae1854024c47ca7661cd72d7f95af2b3f45a87
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-10-03 10:06:22 +0100

    HAAR-1892: ⬆️ update to spring-boot-plugin-5.5.0 (#320)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.5.0"
   kotlin("plugin.spring") version "1.9.10"
 }
 
@@ -8,11 +8,11 @@ configurations {
 }
 
 dependencies {
-  annotationProcessor("org.projectlombok:lombok:1.18.28")
+  annotationProcessor("org.projectlombok:lombok:1.18.30")
 
-  compileOnly("org.projectlombok:lombok:1.18.28")
+  compileOnly("org.projectlombok:lombok:1.18.30")
 
-  runtimeOnly("com.h2database:h2:2.2.222")
+  runtimeOnly("com.h2database:h2:2.2.224")
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:postgresql:42.6.0")
 
@@ -38,14 +38,14 @@ dependencies {
   implementation("com.google.code.gson:gson:2.10.1")
   implementation("com.google.guava:guava:32.1.2-jre")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.547"))
-  implementation("software.amazon.awssdk:sns:2.20.144")
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.560"))
+  implementation("software.amazon.awssdk:sns:2.20.157")
 
-  testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
-  testCompileOnly("org.projectlombok:lombok:1.18.28")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+  testCompileOnly("org.projectlombok:lombok:1.18.30")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.0.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
