89d669573ab93e01ec9114000e649f2e61071b98
petergphillips <peter@greenthistle.co.uk>
2024-02-06 10:15:36 +0000

    CDPS-395: 🎨 Convert event listener test to kotlin (#338)

---
+++ src/test/java/uk/gov/justice/hmpps/casenotes/services/EventListenerTest.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.hmpps.casenotes.services
+
+import com.google.gson.Gson
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.any
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.json.JsonTest
+import wiremock.org.apache.commons.io.IOUtils
+import java.nio.charset.StandardCharsets
+
+@JsonTest
+class EventListenerTest(@Autowired gson: Gson) {
+  private val caseNoteService: CaseNoteService = mock()
+  private val mergeOffenderService: MergeOffenderService = mock()
+  private var eventListener: EventListener = EventListener(caseNoteService, mergeOffenderService, gson)
+
+  @Test
+  fun testDeleteEvent() {
+    whenever(caseNoteService.deleteCaseNotesForOffender(any())).thenReturn(3)
+    eventListener.handleEvents(getJson("offender-deletion-request.json"))
+    verify(caseNoteService).deleteCaseNotesForOffender("A1234AA")
+  }
+
+  @Test
+  fun testMergeEvent() {
+    whenever(mergeOffenderService.checkAndMerge(any())).thenReturn(2)
+    eventListener.handleEvents(getJson("booking-number-changed.json"))
+    verify(mergeOffenderService).checkAndMerge(100001L)
+  }
+
+  private fun getJson(filename: String): String =
+    IOUtils.toString(this::class.java.getResourceAsStream(filename), StandardCharsets.UTF_8.toString())
+}
