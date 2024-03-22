0194d7f21fb2fb26e705c86dd322e24c17616bab
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2024-03-22 14:13:19 +0000

    HAAR-2467: ⬆️ update to spring-boot-plugin-5.15.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
-  kotlin("plugin.spring") version "1.9.22"
+  kotlin("plugin.spring") version "1.9.23"
 }
 
 configurations {
@@ -8,13 +8,13 @@ configurations {
 }
 
 dependencies {
-  annotationProcessor("org.projectlombok:lombok:1.18.30")
+  annotationProcessor("org.projectlombok:lombok:1.18.32")
 
-  compileOnly("org.projectlombok:lombok:1.18.30")
+  compileOnly("org.projectlombok:lombok:1.18.32")
 
   runtimeOnly("com.h2database:h2:2.2.224")
   runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql:42.7.2")
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-cache")
@@ -29,20 +29,20 @@ dependencies {
   implementation("javax.transaction:javax.transaction-api:1.3")
 
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
   implementation("net.sf.ehcache:ehcache:2.10.9.2")
   implementation("org.apache.commons:commons-text:1.11.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.1")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.10.1")
-  implementation("com.google.guava:guava:33.0.0-jre")
+  implementation("com.google.guava:guava:33.1.0-jre")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.667"))
-  implementation("software.amazon.awssdk:sns:2.24.11")
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.685"))
+  implementation("software.amazon.awssdk:sns:2.25.15")
 
-  testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
-  testCompileOnly("org.projectlombok:lombok:1.18.30")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
+  testCompileOnly("org.projectlombok:lombok:1.18.32")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
@@ -50,13 +50,13 @@ dependencies {
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("org.awaitility:awaitility:4.2.0")
-  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
+  testImplementation("org.awaitility:awaitility:4.2.1")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
 
   testImplementation("org.springframework.security.oauth:spring-security-oauth2:2.5.2.RELEASE")
   testImplementation("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v3:2.1.20")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v3:2.1.21")
 }
 
 java {
