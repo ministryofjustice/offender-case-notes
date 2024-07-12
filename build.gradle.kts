457db93ea4c78ca04e4242f8564c61e363e89af1
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-07-12 10:36:21 +0100

    MICN-88

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
-  kotlin("plugin.spring") version "1.9.23"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
+  kotlin("plugin.spring") version "1.9.24"
 }
 
 configurations {
