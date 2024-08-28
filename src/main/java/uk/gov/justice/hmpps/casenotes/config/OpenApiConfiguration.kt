e0efbee421960c055d9fec3ae64b12c7fda35ad7
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-08-28 14:04:19 +0100

    Documentation updates

--- src/main/java/uk/gov/justice/hmpps/casenotes/config/OpenApiConfiguration.kt
+++ src/main/java/uk/gov/justice/hmpps/casenotes/config/OpenApiConfiguration.kt
@@ -4,12 +4,20 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.servers.Server
+import org.springdoc.core.customizers.OperationCustomizer
 import org.springframework.boot.info.BuildProperties
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.expression.BeanFactoryResolver
+import org.springframework.expression.spel.SpelEvaluationException
+import org.springframework.expression.spel.standard.SpelExpressionParser
+import org.springframework.expression.spel.support.StandardEvaluationContext
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.method.HandlerMethod
 
 @Configuration
-class OpenApiConfiguration(buildProperties: BuildProperties) {
+class OpenApiConfiguration(buildProperties: BuildProperties, private val context: ApplicationContext) {
   private val version: String = buildProperties.version
 
   @Bean
@@ -28,4 +36,43 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
         .description("HMPPS Offender Case Notes API")
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
     )
+
+  @Bean
+  fun preAuthorizeCustomizer(): OperationCustomizer {
+    return OperationCustomizer { operation, handlerMethod ->
+      handlerMethod.preAuthorizeForMethodOrClass()?.let {
+        val preAuthExp = SpelExpressionParser().parseExpression(it)
+        val evalContext = StandardEvaluationContext()
+        evalContext.beanResolver = BeanFactoryResolver(context)
+        evalContext.setRootObject(
+          object {
+            fun hasRole(role: String) = listOf(role)
+            fun hasAnyRole(vararg roles: String) = roles.toList()
+          },
+        )
+
+        val roles = try {
+          (preAuthExp.getValue(evalContext) as List<*>).filterIsInstance<String>()
+        } catch (e: SpelEvaluationException) {
+          emptyList()
+        }
+        if (roles.isNotEmpty()) {
+          operation.description =
+            listOf(
+              operation.description ?: "",
+              "Requires one of the following roles: ",
+            ).joinToString(separator = System.lineSeparator()) +
+            roles.joinToString(
+              prefix = "${System.lineSeparator()}* ",
+              separator = "${System.lineSeparator()}* ",
+            )
+        }
+      }
+      operation
+    }
+  }
+
+  private fun HandlerMethod.preAuthorizeForMethodOrClass() =
+    getMethodAnnotation(PreAuthorize::class.java)?.value
+      ?: beanType.getAnnotation(PreAuthorize::class.java)?.value
 }
