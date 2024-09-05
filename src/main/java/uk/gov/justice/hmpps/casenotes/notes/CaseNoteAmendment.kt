049cb4b05d12aed3affff3f06f30a5cac6de6cc9
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-09-05 10:14:43 +0100

    MICN-117 allow null author user id

--- src/main/java/uk/gov/justice/hmpps/casenotes/notes/CaseNoteAmendment.kt
+++ src/main/java/uk/gov/justice/hmpps/casenotes/notes/CaseNoteAmendment.kt
@@ -18,11 +18,10 @@ data class CaseNoteAmendment(
   @NotBlank val authorName: String,
 
   @Schema(
-    required = true,
+    required = false, example = "12345",
     description = "User Id of the user amending the case note - staffId for nomis users, userId for auth users",
-    example = "12345",
   )
-  val authorUserId: String,
+  val authorUserId: String?,
 
   @Schema(required = true, description = "Additional Case Note Information", example = "Some Additional Text")
   @NotBlank val additionalNoteText: String,
