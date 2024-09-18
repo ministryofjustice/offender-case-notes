c294d3b8a7b42fb1a869b8f14f65d81a74d1abea
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-09-18 16:25:33 +0100

    MICN-147 use flag to turn off in prod

--- src/main/java/uk/gov/justice/hmpps/casenotes/events/PersonCaseNoteEventListener.kt
+++ src/main/java/uk/gov/justice/hmpps/casenotes/events/PersonCaseNoteEventListener.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.hmpps.casenotes.events
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue
@@ -14,18 +15,33 @@ class PersonCaseNoteEventListener(
   private val hmppsQueueService: HmppsQueueService,
   private val objectMapper: ObjectMapper,
   private val serviceConfig: ServiceConfig,
+  private val telemetryClient: TelemetryClient,
 ) {
   private val eventTopic by lazy { hmppsQueueService.findByTopicId("domainevents") as HmppsTopic }
 
   @TransactionalEventListener
   fun handlePersonCaseNoteEvent(event: PersonCaseNoteEvent) {
-    eventTopic.publish(
-      event.eventName,
-      objectMapper.writeValueAsString(event.asDomainEvent(serviceConfig.baseUrl)),
-      event.attributes(),
-    )
+    if (serviceConfig.publishPersonEvents) {
+      eventTopic.publish(
+        event.eventName,
+        objectMapper.writeValueAsString(event.asDomainEvent(serviceConfig.baseUrl)),
+        event.attributes(),
+      )
+    } else {
+      telemetryClient.trackEvent(event.eventName, event.properties(), mapOf())
+    }
   }
 
   private fun PersonCaseNoteEvent.attributes() =
     mapOf("eventType" to MessageAttributeValue.builder().dataType("String").stringValue(eventName).build())
+
+  private fun PersonCaseNoteEvent.properties(): Map<String, String> = mapOf(
+    "personIdentifier" to personIdentifier,
+    "id" to id.toString(),
+    "legacyId" to legacyId.toString(),
+    "type" to type,
+    "subType" to subType,
+    "source" to source.toString(),
+    "syncToNomis" to syncToNomis.toString(),
+  )
 }
