eef3dc533d3fbe29b13499f66e202953fb08d335
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-09-23 15:16:52 +0100

    Delete existing legacy case notes for migration

--- src/main/java/uk/gov/justice/hmpps/casenotes/sync/SyncCaseNotes.kt
+++ src/main/java/uk/gov/justice/hmpps/casenotes/sync/SyncCaseNotes.kt
@@ -30,12 +30,17 @@ class SyncCaseNotes(
   private val telemetryClient: TelemetryClient,
 ) {
   fun migrateNotes(toMigrate: List<MigrateCaseNoteRequest>): List<MigrationResult> {
+    val personIdentifiers = toMigrate.map { it.personIdentifier }
+    personIdentifiers.forEach(noteRepository::deleteLegacyCaseNotes)
     val types = getTypesForSync(toMigrate.map { it.typeKey() }.toSet())
-    val existingIds = noteRepository.findMigratedIds(toMigrate.map { it.legacyId })
-    val existingLegacyIds = existingIds.map { it.legacyId }.toSet()
-    val new = toMigrate.filter { it.legacyId !in existingLegacyIds }
-      .map { it.asNoteAndAmendments { t, st -> requireNotNull(types[TypeKey(t, st)]) } }
-    return create(new).map { MigrationResult(it.id, it.legacyId) } + existingIds
+    val new = toMigrate.map { it.asNoteAndAmendments { t, st -> requireNotNull(types[TypeKey(t, st)]) } }
+    return create(new).map { MigrationResult(it.id, it.legacyId) }.also {
+      telemetryClient.trackEvent(
+        "MigrateCaseNotes",
+        mapOf("personIdentifier" to personIdentifiers.toString(), "count" to toMigrate.count().toString()),
+        mapOf(),
+      )
+    }
   }
 
   fun syncNote(request: SyncCaseNoteRequest): SyncResult {
