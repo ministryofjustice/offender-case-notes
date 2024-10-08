d02262dd9572949f06a42634b1c16b98d0ccd240
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-08 14:10:49 +0100

    Update for dependencies

--- src/main/java/uk/gov/justice/hmpps/casenotes/events/PersonCaseNoteEventListener.kt
+++ src/main/java/uk/gov/justice/hmpps/casenotes/events/PersonCaseNoteEventListener.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
-import software.amazon.awssdk.services.sns.model.MessageAttributeValue
 import uk.gov.justice.hmpps.casenotes.config.ServiceConfig
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.HmppsTopic
@@ -25,15 +24,11 @@ class PersonCaseNoteEventListener(
       eventTopic.publish(
         event.eventName,
         objectMapper.writeValueAsString(event.asDomainEvent(serviceConfig.baseUrl)),
-        event.attributes(),
       )
     } else {
       telemetryClient.trackEvent(event.eventName, event.properties(), mapOf())
     }
   }
-
-  private fun PersonCaseNoteEvent.attributes() =
-    mapOf("eventType" to MessageAttributeValue.builder().dataType("String").stringValue(eventName).build())
 
   private fun PersonCaseNoteEvent.properties(): Map<String, String> = mapOf(
     "personIdentifier" to personIdentifier,
