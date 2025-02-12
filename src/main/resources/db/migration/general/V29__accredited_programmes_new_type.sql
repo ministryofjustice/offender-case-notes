b1cc7244157014b5037bc74b662ba0daa22076ce
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-02-12 14:18:54 +0000

    fix names

--- src/main/resources/db/migration/general/V29__accredited_programmes_new_type.sql
+++ src/main/resources/db/migration/general/V29__accredited_programmes_new_type.sql
@@ -1,7 +1,7 @@
 insert
-into case_note_type(id, type_code, code, description, active, created_at, create_by,
+into case_note_sub_type(id, type_code, code, description, active, created_at, created_by,
                     sensitive, restricted_use, sync_to_nomis, dps_user_selectable)
-values (nextval('case_note_type_case_note_type_id_seq'),
+values (nextval('case_note_sub_type_id_seq'),
         'ACP',
         'MVC_BLD',
         'Moved to building choices',
