41017aea0747ffebffdeb5674447348e0af134d8
wcdkj <will.clark@digital.justice.gov.uk>
2025-04-17 14:53:58 +0100

    Updated SearchNotesRequest model schema annotations (#589)

--- src/main/java/uk/gov/justice/hmpps/casenotes/notes/SearchNotesRequest.kt
+++ src/main/java/uk/gov/justice/hmpps/casenotes/notes/SearchNotesRequest.kt
@@ -1,16 +1,20 @@
 package uk.gov.justice.hmpps.casenotes.notes
 
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.hmpps.casenotes.domain.Note
 import java.time.LocalDateTime
 
 data class SearchNotesRequest(
+  @Schema(required = false)
   val includeSensitive: Boolean,
+  @Schema(required = false)
   val typeSubTypes: Set<TypeSubTypeRequest> = emptySet(),
   val occurredFrom: LocalDateTime? = null,
   val occurredTo: LocalDateTime? = null,
   override val page: Int,
   override val size: Int,
+  @Schema(required = false)
   @Parameter(description = "The sort to apply to the results", example = "occurredAt,desc")
   override val sort: String = "${Note.OCCURRED_AT},desc",
 ) : PagedRequest
