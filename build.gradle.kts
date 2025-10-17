8c79813ad4b5461569dc7c3b5279dbae4ccc0602
Scott Rowley <16266605+scottrowley@users.noreply.github.com>
2025-10-17 13:22:34 +0100

    CDPS-1607 Upgrade to Gradle 9.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,6 +76,7 @@ tasks {
     compilerOptions {
       jvmTarget = JvmTarget.JVM_21
       freeCompilerArgs.add("-Xwhen-guards")
+      freeCompilerArgs.add("-Xannotation-default-target=param-property")
     }
   }
 
