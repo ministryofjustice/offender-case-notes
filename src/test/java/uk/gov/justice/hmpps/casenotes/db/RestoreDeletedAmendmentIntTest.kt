5c6860e8995af573e3492e78358535e3c49d6a97
Melia Burnley <melia.burnley@justice.gov.uk>
2026-04-21 12:25:11 +0100

    CDPS-1907: Add restore case note amendment db function

--- src/test/java/uk/gov/justice/hmpps/casenotes/db/RestoreDeletedAmendmentIntTest.kt
+++ src/test/java/uk/gov/justice/hmpps/casenotes/db/RestoreDeletedAmendmentIntTest.kt
@@ -10,20 +10,22 @@ import uk.gov.justice.hmpps.casenotes.controllers.IntegrationTest
 import uk.gov.justice.hmpps.casenotes.controllers.USERNAME
 import uk.gov.justice.hmpps.casenotes.notes.CaseNote
 
-class RestoreDeletedCaseNotesIntTest : IntegrationTest() {
+class RestoreDeletedAmendmentIntTest : IntegrationTest() {
   @Test
   @Sql("classpath:jpa/repository/reset.sql")
+  @Sql("classpath:resources/casenotes/case_notes.sql")
   @Sql("classpath:resources/casenotes/deleted_case_notes.sql")
-  @Sql("classpath:resources/casenotes/restore_deleted_case_notes.sql")
-  fun `can read a restored case note by id`() {
+  @Sql("classpath:resources/casenotes/restore_deleted_case_note_amendment.sql")
+  fun `can read a restored case note amendment on the case note it was associated with`() {
     val response = getCaseNote(
       "A0001AA",
-      "0192007e-abff-7acb-9282-341ffff52e29",
+      "01924e63-4d35-7033-bc72-f0eb8a61b196",
       listOf(SecurityUserContext.ROLE_CASE_NOTES_READ),
     ).success<CaseNote>()
-
-    assertThat(response.id).isEqualTo("0192007e-abff-7acb-9282-341ffff52e29")
-    assertThat(response.amendments.size).isEqualTo(2)
+    assertThat(response.id).isEqualTo("01924e63-4d35-7033-bc72-f0eb8a61b196")
+    assertThat(response.amendments.size).isEqualTo(1)
+    assertThat(response.amendments[0].id.toString()).isEqualTo("0192007f-0ec6-72b8-ad06-509414afaeee")
+    assertThat(response.amendments[0].additionalNoteText).isEqualTo("test amendment 2")
   }
 
   private fun getCaseNote(
