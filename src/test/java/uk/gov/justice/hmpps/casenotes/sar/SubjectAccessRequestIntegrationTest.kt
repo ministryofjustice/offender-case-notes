b55d8c1600c5e527817e893df96ddba817ba00af
Igor <igor.ushkarev1@justice.gov.uk>
2026-04-24 11:11:03 +0100

    Compare flyway migrations to expected versions to ensure that any subsequent changes remind us to maintain SAR compatibility … and drop support for h2 database which is no longer used for testing. Otherwise, the SAR test library thinks there are two migrations with version 5.1.

--- src/test/java/uk/gov/justice/hmpps/casenotes/sar/SubjectAccessRequestIntegrationTest.kt
+++ src/test/java/uk/gov/justice/hmpps/casenotes/sar/SubjectAccessRequestIntegrationTest.kt
@@ -1,12 +1,19 @@
 package uk.gov.justice.hmpps.casenotes.sar
 
 import jakarta.persistence.EntityManager
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
+import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.MigrationState
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
 import org.springframework.security.oauth2.jwt.JwtDecoder
+import uk.gov.justice.digital.hmpps.subjectaccessrequest.SarFlywaySchemaTest
 import uk.gov.justice.digital.hmpps.subjectaccessrequest.SarIntegrationTestHelper
 import uk.gov.justice.digital.hmpps.subjectaccessrequest.SarIntegrationTestHelperConfig
 import uk.gov.justice.digital.hmpps.subjectaccessrequest.SarJpaEntitiesTest
@@ -14,10 +21,12 @@ import uk.gov.justice.hmpps.casenotes.controllers.IntegrationTest
 import uk.gov.justice.hmpps.casenotes.utils.JwtAuthHelper
 import uk.gov.justice.hmpps.test.kotlin.auth.JwtAuthorisationHelper
 import java.time.Duration
+import javax.sql.DataSource
 
 @Import(SarIntegrationTestHelperConfig::class)
 class SubjectAccessRequestIntegrationTest :
   IntegrationTest(),
+  SarFlywaySchemaTest,
   SarJpaEntitiesTest {
 
   @TestConfiguration
@@ -49,11 +58,28 @@ class SubjectAccessRequestIntegrationTest :
   }
 
   @Autowired
+  private lateinit var dataSource: DataSource
+
+  @Autowired
   private lateinit var entityManager: EntityManager
 
   @Autowired
   private lateinit var sarIntegrationTestHelper: SarIntegrationTestHelper
 
+  override fun getDataSourceInstance(): DataSource = dataSource
   override fun getEntityManagerInstance(): EntityManager = entityManager
   override fun getSarHelper(): SarIntegrationTestHelper = sarIntegrationTestHelper
+
+  @Test
+  fun `Flyway schema version should match expected non-future version`(
+    @Value($$"${hmpps.sar.tests.expected-flyway-schema-non-future-version:0}")
+    expectedFlywaySchemaNonFutureVersion: String,
+  ) {
+    // NB: SAR testing library includes test-only migrations so this test case exists to compare main schema only
+    val migrations = Flyway.configure().dataSource(dataSource).load().info().all()
+    val lastMigration = migrations.findLast {
+      it.state == MigrationState.SUCCESS
+    } ?: fail("Last successful non-future migration not found")
+    assertThat(lastMigration.version?.version).`as`("Flyway schema version").isEqualTo(expectedFlywaySchemaNonFutureVersion)
+  }
 }
